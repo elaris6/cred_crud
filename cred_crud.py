98f3eb53e88d0b4e914fb60d4d321a7dda0d55d2
elaris6 <69580384+elaris6@users.noreply.github.com>
2020-08-23 00:21:56 +0200

    Creacion de bbdd solo si no exite previamente

--- cred_crud.py
+++ cred_crud.py
@@ -3,11 +3,14 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-conexion_bbdd = sqlite3.connect("cred_crud.db")
-cursor_bbdd = conexion_bbdd.cursor()
+try:
+    file=open('cred_crud.db','r')
+    file.close()
+    print("La bbdd ya existía. No es necesario crearla.")
+except:
+    conexion_bbdd = sqlite3.connect("cred_crud.db")
+    #cursor_bbdd = conexion_bbdd.cursor()
+    #cursor_bbdd.close()
+    conexion_bbdd.close()
+    print("La bbdd no existía y se ha creado.")
 
-
-
-
-cursor_bbdd.close()
-conexion_bbdd.close()
