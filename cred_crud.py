a761acd58412570d3f4d9bbccba8efadc6bbb119
elaris6 <69580384+elaris6@users.noreply.github.com>
2020-08-23 16:58:03 +0200

    Interfaz principal terminada. Implementadas opciones de salir y ayuda.

--- cred_crud.py
+++ cred_crud.py
@@ -1,18 +1,29 @@
+from sqlite3.dbapi2 import version
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
 import os
 
+version="0.0"
+
 # Función para mostrar información sobre la aplicación en Acerca de...
 def informacion():
     messagebox.showinfo("Almacenamiento usuarios y contraseñas",
-                        "Versión: 0.0\n\nCreada por IBJ como práctica para el curso de YouTube - Python, del canal 'Píldoras Informáticas'")
+                        f"Versión: {version}\n\nCreada por IBJ como práctica para el curso de YouTube - Python, del canal 'Píldoras Informáticas'")
 
 # Función para salir de la aplicación con ventana emergente.
 def salirAplicacion():
     valor = messagebox.askokcancel("Salir de la aplicación", "Quieres salir?")
     if valor == True:
         root.destroy()
+
+# Función para borrar los campos de entrada
+def cleanEntries():
+    identificador.set("")
+    descripcion.set("")
+    usuario.set("")
+    password.set("")
+    textComentarios.delete("1.0",END)
 
 # Función para generar una nueva ventana con los resultados de consulta
 # si los resultados son más de uno
@@ -22,8 +33,23 @@ def ventanaTablaResultados():
 
     ventana_resultados.mainloop()
 
-def boton():
+# Función para tomar los campos de entrada e insertarlos como un nuevo registro almacenado
+def operCreate():
     pass
+
+# Función para tomar el campo de entrada identificador y buscar un registro concreto
+# o tomar el campo descripción y buscar todos los que coincidan con el patrón informado
+def operRead():
+    pass
+
+# Función para tomar los cmapos de entrada y actualizar el registro que coincida con el identificador informado
+def operUpdate():
+    pass
+
+# Función para eliminar el registro que coincida con el identificador informado
+def operDelete():
+    pass
+
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -48,82 +74,91 @@ barraMenu = Menu(root)
 root.config(menu=barraMenu)
 
 # Creamos opción Aplicación de manú principal y sus opciones
-opcion1Menu = Menu(barraMenu, tearoff=0)
+opcion1Menu = Menu(barraMenu, tearoff=False)
 barraMenu.add_cascade(label="Aplicación", menu=opcion1Menu)
 opcion1Menu.add_cascade(label="Eliminar memoria local")
 opcion1Menu.add_separator()  # Separador entre elemenos de menú
 opcion1Menu.add_command(label="Salir", command=salirAplicacion)
 
 # Creamos opción CRUD del menú principal y sus opciones
-opcion2Menu = Menu(barraMenu, tearoff=0)
+opcion2Menu = Menu(barraMenu, tearoff=False)
 barraMenu.add_cascade(label="CRUD", menu=opcion2Menu)
-opcion2Menu.add_command(label="Create")
-opcion2Menu.add_cascade(label="Read",command=ventanaTablaResultados)
-opcion2Menu.add_cascade(label="Update")
-opcion2Menu.add_cascade(label="Delete")
+opcion2Menu.add_command(label="Create",command=operCreate)
+opcion2Menu.add_cascade(label="Read",command=operRead)
+opcion2Menu.add_cascade(label="Update",command=operUpdate)
+opcion2Menu.add_cascade(label="Delete",command=operDelete)
+opcion2Menu.add_separator()  # Separador entre elemenos de menú
+opcion2Menu.add_cascade(label="Limpiar", command=cleanEntries)
 
 # Creamos opción Ayuda del menú principal y sus opciones
-opcion4Menu = Menu(barraMenu, tearoff=0)
-barraMenu.add_cascade(label="Ayuda", menu=opcion4Menu)
-opcion4Menu.add_command(label="Acerca de...",command=informacion)
+opcion3Menu = Menu(barraMenu, tearoff=False)
+barraMenu.add_cascade(label="Ayuda", menu=opcion3Menu)
+opcion3Menu.add_command(label="Acerca de...",command=informacion)
 
 # Creamos frame en el que colocar todas los campos de entrada e información
-frameEntry = Frame(root, width="500", height="500", bg="lightgrey")
+frameEntry = Frame(root, width="500", height="500")
 frameEntry.pack()
 
 identificador = StringVar()
-labelIdentificador = Label(frameEntry, text="Descripción:")
-labelIdentificador.grid(row="0", column="0", sticky="e", pady=5, padx=5)
+labelIdentificador = Label(frameEntry, text="Identificador:")
+labelIdentificador.grid(row=0, column=0, sticky="e", pady=5, padx=5)
 entryIdentificador = Entry(frameEntry, bg="lightgrey", textvariable=identificador)
-entryIdentificador.grid(row="0", column="1", pady=5, padx=5)
+entryIdentificador.grid(row=0, column=1, columnspan=4, pady=5, padx=5)
+entryIdentificador.config(width=40)
 
 descripcion = StringVar()
 labelDescripcion = Label(frameEntry, text="Descripción:")
-labelDescripcion.grid(row="1", column="0", sticky="e", pady=5, padx=5)
-entryDescripcion = Entry(frameEntry, bg="lightgrey", textvariable=descripcion)
-entryDescripcion.grid(row="1", column="1", pady=5, padx=5)
+labelDescripcion.grid(row=1, column=0, sticky="e", pady=5, padx=5)
+entryDescripcion = Entry(frameEntry, textvariable=descripcion)
+entryDescripcion.grid(row=1, column=1, columnspan=4, pady=5, padx=5)
+entryDescripcion.config(width=40)
 
 usuario = StringVar()
-label1 = Label(frameEntry, text="Usuario:")
-label1.grid(row="2", column="0", sticky="e", pady=5, padx=5)
-cuadroTexto1 = Entry(frameEntry, bg="lightgrey", textvariable=usuario)
-cuadroTexto1.grid(row="2", column="1", pady=5, padx=5)
+labelUsuario = Label(frameEntry, text="Usuario:")
+labelUsuario.grid(row=2, column=0, sticky="e", pady=5, padx=5)
+entryUsuario = Entry(frameEntry, textvariable=usuario)
+entryUsuario.grid(row=2, column=1, columnspan=4, pady=5, padx=5)
+entryUsuario.config(width=40)
 
 password = StringVar()
-label2 = Label(frameEntry, text="Password:")
-label2.grid(row="3", column="0", sticky="e", pady=5, padx=5)
-cuadroTexto2 = Entry(frameEntry, bg="lightgrey", textvariable=password)
-cuadroTexto2.grid(row="3", column="1", pady=5, padx=5)
-cuadroTexto2.config(show="*")
+labelPassword = Label(frameEntry, text="Password:")
+labelPassword.grid(row=3, column=0, sticky="e", pady=5, padx=5)
+entryPassword = Entry(frameEntry, textvariable=password)
+entryPassword.grid(row=3, column=1, columnspan=4, pady=5, padx=5)
+entryPassword.config(show="*")
+entryPassword.config(width=40)
 
-label3 = Label(frameEntry, text="Comentarios:")
-label3.grid(row="4", column="0", sticky="e", pady=5, padx=5)
-text3 = Text(frameEntry, width="20", height="5")
-text3.grid(row="4", column="1", pady="5", padx="5")
-scroll3 = Scrollbar(frameEntry, command=text3.yview)
-scroll3.grid(row="4", column="2", sticky="nsew")
-text3.config(yscrollcommand=scroll3.set)
+labelComentarios = Label(frameEntry, text="Comentarios:")
+labelComentarios.grid(row=4, column=0, sticky="e", pady=5, padx=5)
+textComentarios = Text(frameEntry, width="20", height="5")
+textComentarios.grid(row=4, column=1, columnspan=3, pady="5", padx="5")
+scrollComentarios = Scrollbar(frameEntry, command=textComentarios.yview)
+scrollComentarios.grid(row=4, column=4, sticky="nsew")
+textComentarios.config(width=27, yscrollcommand=scrollComentarios.set)
 
 
 # Creamos frame en el que colocar los botones CRUD
-frameButtons = Frame(root, width="500", height="100", bg="grey")
+frameButtons = Frame(root, width="500", height="100", bg="lightgrey")
 frameButtons.pack()
 
 # Creamos botones de acción CRUD
-botonClean = Button(frameButtons, text="Limpiar", command=boton)
-botonClean.grid(row=0, column=0, pady=5, padx=5)
+botonClean = Button(frameButtons, text="Limpiar", width=8, height=2, command=cleanEntries)
+botonClean.grid(row=0, column=0, pady=15, padx=10)
 
-botonCreate = Button(frameButtons, text="Create", command=boton)
-botonCreate.grid(row=0, column=1, pady=5, padx=5)
+botonCreate = Button(frameButtons, text="Create", width=8, height=2, command=operCreate)
+botonCreate.grid(row=0, column=1, pady=15, padx=10)
 
-botonRead = Button(frameButtons, text="Read", command=boton)
-botonRead.grid(row=0, column=2, pady=5, padx=5)
+botonRead = Button(frameButtons, text="Read", width=8, height=2, command=operRead)
+botonRead.grid(row=0, column=2, pady=15, padx=10)
 
-botonUpdate = Button(frameButtons, text="Update", command=boton)
-botonUpdate.grid(row=0, column=3, pady=5, padx=5)
+botonUpdate = Button(frameButtons, text="Update", width=8, height=2, command=operUpdate)
+botonUpdate.grid(row=0, column=3, pady=15, padx=1)
 
-botonDelete = Button(frameButtons, text="Delete", command=boton)
-botonDelete.grid(row = 0, column = 4, pady = 5, padx = 5)
+botonDelete = Button(frameButtons, text="Delete", width=8, height=2, command=operDelete)
+botonDelete.grid(row = 0, column = 4, pady = 15, padx = 10)
+
+labelVersion = Label(frameButtons, bg="lightgrey", text=f"Version: {version}")
+labelVersion.grid(row = 1, column = 4, pady = 5, padx = 5)
 
 
 root.mainloop()
